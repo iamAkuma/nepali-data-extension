97e59901b379a37594be2c10e93fccb34a535777
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-19 17:07:26 +0545

    Solved some date showing problems

--- popup.js
+++ popup.js
@@ -1,18 +1,16 @@
 window.addEventListener('DOMContentLoaded', function () {
-    const nepaliDateElement = document.getElementById('nepali-date');
-  
-    function getNepaliDate() {
-      const today = new Date();
-      const nepaliDate = new NepaliDate(today);
-  
-      const formattedDate = nepaliDate.format('dddd, DD MMMM YYYY', 'np');
-      const sanitizedDate = formattedDate.replace('२,', ','); // Remove the digit "२" followed by comma
-  
-      return sanitizedDate;
-    }
-  
-    // Display today's Nepali date on page load
-    const nepaliDate = getNepaliDate();
-    nepaliDateElement.innerText = nepaliDate;
-  });
-  
+  const nepaliDateElement = document.getElementById('nepali-date');
+
+  function getNepaliDate() {
+    const today = new Date();
+    const nepaliDate = new NepaliDate(today);
+
+    const formattedDate = nepaliDate.format('dddd, DD MMMM YYYY', 'np');
+    const sanitizedDate = formattedDate.replace(/([०१२३४५६७८९]),/g, ',');
+
+    return sanitizedDate;
+  }
+
+  // Display today's Nepali date on page load
+  nepaliDateElement.textContent = getNepaliDate();
+});
